2af677f8f885e1c428c0ff826d6cf6f375c48a96
nghiane <nghianguyentrong@tcomglobal.vn>
2025-12-01 16:24:39 +0700

    Initial Commit

---
+++ DeliveryApp/DeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppApp.swift
+//  DeliveryApp
+//
+//  Created by Trọng Nghĩa Nguyễn on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
